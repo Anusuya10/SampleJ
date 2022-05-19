c30215bad177d0159332a2c89a99e617cfc2c963
Anusuya10 <anubbytaj2510@gmail.com>
2022-05-19 17:14:21 +0530

    Vishal

---
+++ src/org/college/College.java
@@ -0,0 +1,31 @@
+package org.college;
+
+public class College {
+	private void collegeName() {
+		System.out.println("American University");
+
+	}
+	private void collegeCode() {
+		System.out.println("253535");
+
+	}
+	private void collegeRank() {
+		System.out.println("1 st Rank");
+
+	}
+	public static void main(String[] args) {
+		College a = new College();
+				a.collegeName();
+				a.collegeCode();
+				a.collegeRank();
+				Student b = new Student();
+				b.studentName();
+				b.studentDept();
+				b.studentId();
+				Hostel c = new Hostel();
+						c.hostelName();
+						Dept d = new Dept();
+						d.deptName();
+	}
+
+}
---
+++ src/org/college/Student.java
@@ -0,0 +1,17 @@
+package org.college;
+
+public class Student {
+	public void studentName() {
+		System.out.println("Anusuya");
+
+	}
+	public void studentDept() {
+		System.out.println("ECE");
+
+	}
+	public void studentId() {
+		System.out.println("ECE 251004");
+
+	}
+
+}
